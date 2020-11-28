bd5d033608edfe5a24d56647c654d1295fe22611
YukiFujitsu <hamukawa0406@gmail.com>
2020-11-29 01:47:59 +0900

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   log.txt 	new file:   reeport.aux 	new file:   reeport.bbl 	new file:   reeport.blg 	new file:   reeport.log 	modified:   reeport.pdf 	new file:   reeport.synctex.gz 	modified:   reeport.tex 	new file:   report.docx 	new file:   sample_thread.py 	new file:   sequenceV2.png 	new file:   telloDoc/Tello EDU Scratch README 0507.pdf 	new file:   telloDoc/Tello Mission Pad User Guide.pdf 	new file:   telloDoc/Tello SDK 2.0 User Guide.pdf 	new file:   telloDoc/Tello User Manual v1.4.pdf 	new file:   telloDoc/Tello+Disclaimer+and+Safety+Guidelines+(EN)+v1.0.pdf 	new file:   telloDoc/Tello+Quick+Start+Guide_V1.2+multi.pdf

---
+++ sample_thread.py
@@ -0,0 +1,23 @@
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+def make_udon(kind):
+    print('  %sうどんを作ります。' % kind)
+    time.sleep(5)
+    return kind + 'うどん'
+
+kinds = ['たぬき', 'かけ', 'ざる', 'きつね', '天ぷら', '肉']
+executor = ThreadPoolExecutor(max_workers=3)
+futures = []
+
+while True:
+    for kind in kinds:
+        print('%sうどん オーダー入りました。' % kind)
+        future = executor.submit(make_udon, kind)
+        futures.append(future)
+
+    for future in futures:
+        if future.done() == True:
+            print('%sお待たせしました。' % future.result())
+ 
+executor.shutdown()
